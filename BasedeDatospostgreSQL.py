a1c65e32269e1dfb00f19b1ada8a7b6bbc31d21d
GuillermoCioli <110001390+GuillermoCioli@users.noreply.github.com>
2022-07-25 23:35:23 -0300

    Add files via upload

---
+++ BasedeDatospostgreSQL.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 25 23:26:01 2022
+
+@author: Laptop ASUS
+"""
+
+from sqlalchemy import create_engine
+from ObtencionyExtracciondeDatos import tabla_unica, col_prov, df_reg_prov
+
+engine = create_engine('postgresql://postgres:12345@localhost:5432/alkemy')
+
+tabla_unica.to_sql('tabla_unificada', con=engine, if_exists='replace')
+col_prov.to_sql('cantidades_cines', con=engine, if_exists='replace')
+df_reg_prov.to_sql('categorias_fuentes', con=engine, if_exists='replace')
